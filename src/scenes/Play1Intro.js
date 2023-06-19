5e1f8c8fcdde041037a43bd6e0dad18fa15da0ae
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-06-18 17:49:27 -0700

    added intro scenes

---
+++ src/scenes/Play1Intro.js
@@ -0,0 +1,28 @@
+class Play1Intro extends Phaser.Scene{
+    constructor(){
+        super('play1IntroScene')
+    }
+
+    preload(){
+
+    }
+
+    create(){
+        // define keys
+        keyM = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M)
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+    }
+
+    update(){
+        if (Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart() 
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyM)) {
+            this.scene.start('menuScene') 
+        }
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            this.scene.start('play1Scene') 
+        }
+    }
+}
---
+++ src/scenes/Play2Intro.js
@@ -0,0 +1,24 @@
+class Play2Intro extends Phaser.Scene{
+    constructor(){
+        super('play2IntroScene')
+    }
+
+    preload(){
+
+    }
+
+    create(){
+        // define keys
+        keyM = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M)
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
+    }
+
+    update(){
+        if (Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart() 
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyM)) {
+            this.scene.start('menuScene') 
+        }
+    }
+}
